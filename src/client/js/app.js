c29fe7872f3ea41c88cea7b6dd40bae4de6d8676
Nguyen Cong Duy <ncongduy@gmail.com>
2021-10-26 07:50:14 +0300

    create feature: add 'back to top' button

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,13 +1,21 @@
-function submitForm(evt) {
+function scrollToElement(evt) {
 	evt.preventDefault();
 
-	const infoElement = document.getElementById('info');
-	infoElement.scrollIntoView({ behavior: 'smooth' });
+	const scrollToElement = document.getElementById(evt.target.dataset.scroll);
+	scrollToElement.scrollIntoView({ behavior: 'smooth' });
 }
 
 function app() {
-	const searchBtn = document.getElementById('search-btn');
-	searchBtn.addEventListener('click', submitForm);
+	const $ = document.querySelector.bind(document);
+	const $$ = document.querySelectorAll.bind(document);
+
+	// add event listener for search button
+	const searchBtn = $('#search-btn');
+	searchBtn.addEventListener('click', scrollToElement);
+
+	// add event listener for 'back to top' button
+	const backToTop = $('.back-to-top');
+	backToTop.addEventListener('click', scrollToElement);
 }
 
 export { app };
