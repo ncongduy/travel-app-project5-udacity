d594b7f102e3c559f2e0d60112dbfb2142005de1
Nguyen Cong Duy <ncongduy@gmail.com>
2021-10-28 18:10:41 +0300

    add feature: work offline by using Services Workers

--- src/server/index.js
+++ src/server/index.js
@@ -61,8 +61,8 @@ console.log(__dirname);
 
 // routing
 app.get('/', function (req, res) {
-	// res.sendFile('dist/index.html');
-	res.sendFile(path.resolve('src/client/views/index.html'));
+	res.sendFile('dist/index.html');
+	// res.sendFile(path.resolve('src/client/views/index.html'));
 });
 
 app.get('/test', function (req, res) {
